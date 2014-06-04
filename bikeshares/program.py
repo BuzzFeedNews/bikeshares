66bbcf382dc6dae5811b6997547519f64aa4ba86
Jeremy Singer-Vine <jsvine@gmail.com>
2014-06-04 12:47:16 -0400

    Separate TripSubset & dataframe, tweak time slices

--- bikeshares/program.py
+++ bikeshares/program.py
@@ -1,9 +1,19 @@
 import pandas as pd
 
-class TripSubset(pd.DataFrame):
+class TripSubset(object):
+    aliased_methods = [
+        "head", "tail",
+        "to_csv", "to_json", "to_dict", "get_values"
+    ]
+
+    def __init__(self, frame):
+        self.df = pd.DataFrame(frame)
+        for m in self.aliased_methods:
+            setattr(self, m, getattr(self.df, m))
+
     def by_station(self):
-        started = self.groupby("start_station").size()
-        ended = self.groupby("end_station").size()
+        started = self.df.groupby("start_station").size()
+        ended = self.df.groupby("end_station").size()
         counts = pd.DataFrame({
             "trips_started": started,
             "trips_ended": ended,
@@ -12,20 +22,31 @@ class TripSubset(pd.DataFrame):
         return counts.reindex(counts.index.rename("station_id")).reset_index()
 
     def by_interval(self, interval, interval_name):
-        counts = self.set_index("start_time")["bike_id"]\
+        counts = self.df.set_index("start_time")["bike_id"]\
             .resample(interval, how=len)
         return pd.DataFrame({
             "trips_total": counts
         }).reindex(counts.index.rename(interval_name))
 
-    def by_day(self): return self.by_interval("D", "day")
+    def by_day(self): return self.df.by_interval("D", "day")
 
-    def by_month(self): return self.by_interval("MS", "month")
+    def by_month(self): return self.df.by_interval("MS", "month")
 
     def get_time_range(self, event="start"):
-        times = self[event + "_time"]
+        times = self.df[event + "_time"]
         return (times.min(), times.max())
+
+    def from_time(self, time_ref, event="start"):
+        selection = self.df.set_index(event + "_time").ix[time_ref:]
+        return TripSubset(selection.reset_index())
     
+    def to_time(self, time_ref, event="start"):
+        selection = self.df.set_index(event + "_time").ix[:time_ref]
+        return TripSubset(selection.reset_index())
+
+    def between_times(self, t1, t2, event="start"):
+        return self.from_time(t1, event).to_time(t2, event)
+
 class BikeShareProgram(object):
     def __init__(self):
         self.trips = None
